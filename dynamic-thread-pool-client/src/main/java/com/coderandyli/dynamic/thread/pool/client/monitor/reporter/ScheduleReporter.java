4f3583e73579cae5b104527c4c727e82d495833f
lizhenzhen <zhenzhen@e-ports.com>
2021-08-26 18:51:45 +0800

    add README.md

--- dynamic-thread-pool-client/src/main/java/com/coderandyli/dynamic/thread/pool/client/monitor/reporter/ScheduleReporter.java
+++ dynamic-thread-pool-client/src/main/java/com/coderandyli/dynamic/thread/pool/client/monitor/reporter/ScheduleReporter.java
@@ -3,6 +3,15 @@ package com.coderandyli.dynamic.thread.pool.client.monitor.reporter;
 import com.coderandyli.dynamic.thread.pool.client.monitor.Aggregator;
 import com.coderandyli.dynamic.thread.pool.client.monitor.TaskStat;
 import com.coderandyli.dynamic.thread.pool.client.monitor.ThreadTaskInfo;
+import com.coderandyli.dynamic.thread.pool.client.monitor.alert.Alert;
+import com.coderandyli.dynamic.thread.pool.client.monitor.alert.AlertRule;
+import com.coderandyli.dynamic.thread.pool.client.monitor.alert.StatInfo;
+import com.coderandyli.dynamic.thread.pool.client.monitor.alert.handler.ActiveAlertHandler;
+import com.coderandyli.dynamic.thread.pool.client.monitor.alert.handler.AlertHandler;
+import com.coderandyli.dynamic.thread.pool.client.monitor.alert.handler.TaskRejectAlertHandler;
+import com.coderandyli.dynamic.thread.pool.client.monitor.alert.notification.NormalNotification;
+import com.coderandyli.dynamic.thread.pool.client.monitor.alert.sender.ConsoleMsgSender;
+import com.coderandyli.dynamic.thread.pool.client.monitor.alert.sender.MsgSender;
 import com.coderandyli.dynamic.thread.pool.client.monitor.metrics.MetricsStorage;
 import com.coderandyli.dynamic.thread.pool.client.monitor.view.StatViewer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +32,8 @@ public abstract class ScheduleReporter {
     private static final long MAX_STAT_DURATION_IN_MILLIS = 10 * 60 * 1000; // 10minutes
     protected Aggregator aggregator;
 
+    private Alert alert;
+
     @Autowired
     protected MetricsStorage metricsStorage;
 
@@ -31,11 +42,32 @@ public abstract class ScheduleReporter {
 
     public ScheduleReporter() {
         this.aggregator = new Aggregator();
+
+        // 预警相关配置
+        MsgSender msgSender = new ConsoleMsgSender();
+        NormalNotification normalNotification = new NormalNotification(msgSender);
+        AlertRule alertRule = new AlertRule();
+        AlertHandler activeAlertHandler = new ActiveAlertHandler(normalNotification, alertRule);
+        AlertHandler taskRejectAlertHandler = new TaskRejectAlertHandler(normalNotification, alertRule);
+
+        alert = new Alert();
+        alert.addAlertHandler(activeAlertHandler);
+        alert.addAlertHandler(taskRejectAlertHandler);
     }
 
     protected void doStatAndReport(long startTimeInMillis, long endTimeInMillis) {
         Map<String, TaskStat> stats = doStat(startTimeInMillis, endTimeInMillis);
         viewer.output(stats, startTimeInMillis, endTimeInMillis);
+
+        // 触发预警逻辑
+        // TODO: 2021/8/26 完善
+        StatInfo statInfo = StatInfo.builder()
+                .threadPoolId("test-application:thread-pool")
+                .activeCount(10)
+                .maximumPoolSize(15)
+                .rejectCount(2)
+                .build();
+        alert.check(statInfo);
     }
 
     /**
